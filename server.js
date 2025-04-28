b8ff3898754daf740e98113d65babef63ddf8243
Khoa <khoaho120703@gmail.com>
2025-04-28 16:01:46 +0700

    [ADD]_Cors COnfig

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ require("dotenv").config();
 const dbConnect = require("./config/dbconnect");
 const initRouter = require("./routes");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
+app.use(
+  cors({
+    origin: process.env.URL_CLIENT,
+    methods: ["POST", "GET", "PUT", "DELETE"],
+  })
+);
 app.use(cookieParser());
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
